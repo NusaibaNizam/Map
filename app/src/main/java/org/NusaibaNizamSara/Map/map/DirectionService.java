7ac078b74033932dc1f23fcc4413943d3e8b44ff
NusaibaNizam <saranusaibanizam@gmail.com>
2021-01-08 08:17:44 +0600

    done

---
+++ app/src/main/java/org/NusaibaNizamSara/Map/map/DirectionService.java
@@ -0,0 +1,15 @@
+package org.NusaibaNizamSara.Map.map;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
+/**
+ * Created by BITM Trainer 601 on 1/17/2018.
+ */
+
+public interface DirectionService {
+
+    @GET
+    Call<DirectionResponse>getDirections(@Url String urlString);
+}
